6dd6310c1cedb3222054540006f491e96bc4e54b
iratatui <andreikarpenkin123@gmail.com>
2025-05-02 11:25:36 +0300

    add func welcome_user

---
+++ brain_games/cli.py
@@ -0,0 +1,7 @@
+from prompt import string
+
+
+def welcome_user():
+    name = string('May I have your name? ')
+    print(f'Hello, {name}!')
+    
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,9 +1,12 @@
+from brain_games import cli
+
 def greeting():
     print('Welcome to the Brain Games!')
     
 
 def main():
     greeting()
+    cli.welcome_user()
 
 
 if __name__ == '__main__':
