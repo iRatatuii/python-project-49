cdb75e72a9480f99eec58780bc815982c401b10d
iratatui <andreikarpenkin123@gmail.com>
2025-05-03 08:12:20 +0300

    refactor: move game logic to engine module

---
+++ brain_games/engine.py
@@ -0,0 +1,33 @@
+from prompt import string
+
+from brain_games import cli
+from brain_games.scripts.brain_games import greeting
+
+
+def run_game(game_description, generate_round):
+    user_name = cli.welcome_user()
+    greeting()
+    game_rules = game_description
+    print(game_rules)
+    rounds_to_win = 3
+    
+    for i in range(rounds_to_win):
+        question, correct_answer = generate_round()
+        print(f'Question: {question}')
+        user_answer = string('Your answer: ')
+        if user_answer == correct_answer:
+            print('Correct!')
+        else:
+            result = (
+                f"'{user_answer}' is wrong answer ;(."
+                f"Correct answer was '{correct_answer}'.!"
+                      )
+            print(result)
+            print(f"Let's try again, {user_name}!")
+    else:
+        print(f'Congratulations, {user_name}!')
+
+
+if __name__ == '__main__':
+    
+    run_game()
--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,40 +1,18 @@
 from random import randint
 
-from prompt import string
+from brain_games.engine import run_game
 
-from brain_games import cli
-from brain_games.scripts.brain_games import greeting
+
+def get_description():
+    return ('Answer "yes" if the number is even, '
+            'otherwise answer "no".')
+
+
+def generate_round():
+    random_number = randint(2, 100)
+    correct_answer = 'yes' if random_number % 2 == 0 else 'no'
+    return str(random_number), correct_answer
 
 
 def main():
-    user_name = cli.welcome_user()
-    greeting()
-    game_rules = ('Answer "yes" if the number is even, '
-                  'otherwise answer "no".')
-    print(game_rules)
-    flag = True
-    count_games = 0
-    
-    while count_games < 3 and flag:
-        random_number = randint(2, 100)
-        correct_answer = 'yes' if random_number % 2 == 0 else 'no'
-        print(f'Question: {random_number}')
-        user_answer = string('Your answer: ')
-        if user_answer == correct_answer:
-            print('Correct!')
-            count_games += 1
-            if count_games == 3:
-                print(f'Congratulations, {user_name}!')
-        else:
-            result = (
-                f"'{user_answer}' is wrong answer ;(."
-                f"Correct answer was '{correct_answer}'.!"
-                      )
-            print(result)
-            print(f"Let's try again, {user_name}!")
-            flag = False
-    
-    
-if __name__ == '__main__':
-    
-    main()
+    run_game(get_description(), generate_round)
