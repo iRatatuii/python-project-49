573499190b8e6ead695f56745b9a591d9246a8cc
iratatui <andreikarpenkin123@gmail.com>
2025-05-03 11:00:09 +0300

    feat: add new game brain-calc

---
+++ brain_games/scripts/brain_calc.py
@@ -0,0 +1,27 @@
+from brain_games.engine import run_game
+from random import choice, randint
+from operator import add, mul, sub
+
+
+def get_description():
+    return 'What is the result of the expression?'
+
+
+def generate_round():
+    
+    operations = {
+        '+': add,
+        '-': sub,
+        '*': mul
+    }
+    
+    operator = choice(list(operations.keys()))
+    num1 = randint(1, 100) if operator != '*' else randint(2, 10)
+    num2 = randint(1, 100) if operator != '*' else randint(2, 10)
+    quesion = f'{num1} {operator} {num2}'
+    correct_answer = operations[operator](num1, num2)
+    print(correct_answer)
+    return quesion, str(correct_answer)
+
+def main():
+    run_game(get_description(), generate_round)
